36cc71ea290c84294b105b01007dddbeec893bb2
flyri0 <franciscocandidolyrio@gmail.com>
2025-07-19 21:47:32 -0300

    Update BasePage to use `ttkbootstrap` and refactor `AppController` reference to `App`.

--- pages/base.py
+++ pages/base.py
@@ -1,13 +1,13 @@
-import tkinter.ttk as ttk
+import ttkbootstrap as ttk
 from typing import TYPE_CHECKING
 
 from logger import AppLogger
 
 if TYPE_CHECKING:
-    from main import AppController
+    from main import App
 
 class BasePage(ttk.Frame):
-    def __init__(self, parent, controller: 'AppController'):
+    def __init__(self, parent, controller: 'App'):
         super().__init__(parent)
         self.controller = controller
         self.logger = AppLogger.get_logger()
