84a20cda9f846bd745427b146059a142cba7f58e
flyri0 <franciscocandidolyrio@gmail.com>
2025-07-23 21:44:23 -0300

    Refactor `WelcomePage`: add `ScaledLabel` for responsive banner image, update layout alignment, and refine message styling.

--- pages/welcome.py
+++ pages/welcome.py
@@ -1,7 +1,8 @@
 import gettext
 
-from PySide6.QtCore import Qt, QMargins
-from PySide6.QtWidgets import QLabel, QHBoxLayout
+from PySide6.QtCore import Qt, QMargins, QSize
+from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QLabel, QHBoxLayout, QSizePolicy
 
 from pages.base import BasePage
 
@@ -24,10 +25,8 @@ class WelcomePage(BasePage):
         # QLabel support some HTML tags: https://doc.qt.io/qt-6/richtext-html-subset.html
         message = QLabel(_(
             """
-            <center>
-                <h2>Until Then... <i>In portuguese!</i></h2>
-            </center
-            <p align="left">
+            <h2>Until Then... <i>In portuguese!</i></h2>
+            <p>
                 Thank you for being here! This translation was made with love
                 by fans, so that more people can experience the story of <i>Until Then</i>
                 in our language, We hope you get as emotional as we did.
@@ -51,7 +50,36 @@ class WelcomePage(BasePage):
         ))
         message.setWordWrap(True)
         message.setOpenExternalLinks(True)
-        message.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        message.setAlignment(Qt.AlignmentFlag.AlignVCenter | Qt.AlignmentFlag.AlignLeft)
         message.setContentsMargins(QMargins(10, 0, 10, 0))
 
-        main_layout.addWidget(message)
+        banner = ScaledLabel()
+        banner.setPixmap(QPixmap(":/assets/banner.jpg"))
+
+        main_layout.addWidget(banner)
+        main_layout.addWidget(message, stretch=1)
+
+class ScaledLabel(QLabel):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self._pixmap = None
+        self.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.setSizePolicy(
+            QSizePolicy.Policy.Expanding,
+            QSizePolicy.Policy.Expanding
+        )
+
+    def setPixmap(self, pixmap: QPixmap):
+        self._pixmap = pixmap
+        super().setPixmap(pixmap)
+
+
+    def resizeEvent(self, event):
+        if self._pixmap:
+            scaled = self._pixmap.scaled(
+                QSize(int(self._pixmap.width() * self.height() / self._pixmap.height()), self.height()),
+                Qt.AspectRatioMode.KeepAspectRatio,
+                Qt.TransformationMode.SmoothTransformation
+            )
+            super().setPixmap(scaled)
+        super().resizeEvent(event)
