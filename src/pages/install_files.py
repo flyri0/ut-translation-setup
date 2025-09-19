0a2411de18569a73006784f627b21a9cb660c5ef
flyri0 <franciscocandidolyrio@gmail.com>
2025-09-18 21:23:50 -0300

    wip: add logging and extraction feedback to install files page

--- src/pages/install_files.py
+++ src/pages/install_files.py
@@ -3,8 +3,9 @@ import zipfile
 from pathlib import Path
 from typing import Optional
 
-from PySide6.QtCore import Signal, QObject, QThread, QFile, QTemporaryDir
-from PySide6.QtWidgets import QWidget
+from PySide6.QtCore import Signal, QObject, QThread, QFile, QTemporaryDir, Qt
+from PySide6.QtWidgets import QWidget, QTextEdit, QVBoxLayout, QProgressBar, \
+    QLabel
 
 
 class InstallFilesPage(QWidget):
@@ -14,13 +15,44 @@ class InstallFilesPage(QWidget):
         super().__init__()
         self._target_path: Optional[Path] = None
         self._is_demo: Optional[bool] = None
+        self._total_files = 0
         self.temp_dir = QTemporaryDir()
+        self._ui()
+
+    def _ui(self):
+        layout = QVBoxLayout(self)
+        layout.setContentsMargins(100, 0, 100, 0)
+
+        self.status_label = QLabel()
+        self.status_label.setText(self.tr("Extracting Files..."))
+        self.status_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+
+        self.progress_bar = QProgressBar()
+
+        self.log_widget = _LogWidget()
+
+        layout.addStretch()
+        layout.addWidget(self.status_label)
+        layout.addWidget(self.progress_bar)
+        layout.addWidget(self.log_widget)
+        layout.addStretch()
 
     def set_target_path(self, target_path: Path):
         self._target_path = target_path
 
     def set_is_demo(self, is_demo: bool):
         self._is_demo = is_demo
+
+    def _on_total_files(self, total_files: int):
+        self.progress_bar.setRange(0, total_files)
+        self._total_files = total_files
+
+    def _on_progress(self, count: int, name: str):
+        self.status_label.setText(self.tr(
+            f"Extracting {count} of {self._total_files}"
+        ))
+        self.progress_bar.setValue(count)
+        self.log_widget.append_message(name)
 
     def _unzip(
             self,
@@ -36,12 +68,15 @@ class InstallFilesPage(QWidget):
         file.close()
 
         self.unzip_thread = QThread(self)
-        self.unzip_worker = UnzipWorker()
+        self.unzip_worker = _UnzipWorker()
 
         self.unzip_worker.moveToThread(self.unzip_thread)
         self.unzip_thread.started.connect(
             lambda: self.unzip_worker.run(data, destination, folder_name)
         )
+
+        self.unzip_worker.total_files.connect(self._on_total_files)
+        self.unzip_worker.progress.connect(self._on_progress)
 
         self.unzip_worker.finished.connect(self.unzip_thread.quit)
         self.unzip_worker.finished.connect(self.unzip_worker.deleteLater)
@@ -50,7 +85,7 @@ class InstallFilesPage(QWidget):
         self.unzip_thread.start()
 
 
-class UnzipWorker(QObject):
+class _UnzipWorker(QObject):
     finished = Signal()
     error = Signal(Exception)
     total_files = Signal(int)
@@ -82,3 +117,18 @@ class UnzipWorker(QObject):
             self.error.emit(e)
             self.finished.emit()
             return None
+
+
+class _LogWidget(QTextEdit):
+    def __init__(self):
+        super().__init__()
+
+        self.setReadOnly(True)
+        self.setLineWrapMode(QTextEdit.LineWrapMode.NoWrap)
+        self.viewport().setCursor(Qt.CursorShape.ArrowCursor)
+
+    def append_message(self, message: str):
+        self.append(message)
+
+        scrollbar = self.verticalScrollBar()
+        scrollbar.setValue(scrollbar.maximum())
